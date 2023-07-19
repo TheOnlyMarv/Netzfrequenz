8442abc90cbc00ff87dfe9b5fe53f01f6a461c10
AlexEe <alexandra.esche@gmx.de>
2023-07-19 14:07:17 +0200

    Replace for loop with array 'some' method

--- client/src/app/pages/frequency-chart/frequency-chart.component.ts
+++ client/src/app/pages/frequency-chart/frequency-chart.component.ts
@@ -18,7 +18,7 @@ export class FrequencyChartComponent implements OnInit {
   constructor(public service: FrequencyChartService) {}
 
   loadChart(): void {
-    this.service.getFrequencyChartInfo(10).subscribe((response) => {
+    this.service.getFrequencyChartInfo(60).subscribe((response) => {
 
       if(response == undefined) 
       {
@@ -28,11 +28,7 @@ export class FrequencyChartComponent implements OnInit {
         const labeldata: any[] = response?.map((x, i) => i == 0 ? 'jetzt' : formatDate(new Date(x.Timestamp), "HH:mm:ss", "de-DE").toString());
         const realdata: any[] = response?.map(x => x.Frequency);
         var showEmergencyMeasures = false;
-        for (let freq of realdata) {
-          if (freq > 51.5 || freq < 49) {
-            showEmergencyMeasures = true;
-          }
-        }
+        showEmergencyMeasures = realdata.some(x => (x > 51.5 || x < 49));
         this.createChart(labeldata.reverse(), realdata.reverse(), showEmergencyMeasures);
       }
     });
@@ -61,137 +57,87 @@ export class FrequencyChartComponent implements OnInit {
     )
     }
 
+    var datasetData = [
+      {
+        label: "Netzfrequenz",
+        data: realdata,
+        backgroundColor: "rgba(0,0,0.7)",
+        borderColor: "rgba(0,0,0,0.6)"
+      },
+      {
+        label: "Richtwert",
+        data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50),
+        fill: false,
+        pointRadius: 0.1,
+        borderColor: "rgba(50,205,50,0.8)",
+        backgroundColor: "rgba(50,205,50,0.8)",
+      },
+      {
+        label: "Start Regelleistung (negativ)",
+        data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.02),
+        fill: false,
+        pointRadius: 0.1,
+        borderColor: "rgba(255,165,0,0.5)",
+        backgroundColor: "rgb(251, 244, 226)",
+        borderDash: [5]
+      },
+      {
+        label: "Start Regelleistung (positiv)",
+        data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.98),
+        fill: false,
+        pointRadius: 0.1,
+        borderColor: "rgba(255,165,0,0.5)",
+        backgroundColor: "rgb(251, 244, 226)",
+        borderDash: [20, 5]
+      },
+      {
+        label: "Reduzierung der Einspeisung",
+        data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.2),
+        fill: false,
+        pointRadius: 0.1,
+        borderColor: "rgba(255,99,71,0.7)",
+        backgroundColor: "rgb(251, 244, 226)",
+        borderDash: [5]
+      },
+      {
+        label: "Einspeisung von Leistungsreserven",
+        data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.8),
+        fill: false,
+        pointRadius: 0.1,
+        borderColor: "rgba(255,99,71,0.7)",
+        backgroundColor: "rgb(251, 244, 226)",
+        borderDash: [20, 5]
+      },
+    ]
+
     if (showEmergencyMeasures) {
-      this.chart.data = {
-        labels: labeldata, 
-        datasets: [
-          {
-            label: "Netzfrequenz",
-            data: realdata,
-            backgroundColor: "rgba(0,0,0.7)",
-            borderColor: "rgba(0,0,0,0.6)"
-          },
-          {
-            label: "Richtwert",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(50,205,50,0.8)",
-            backgroundColor: "rgba(50,205,50,0.8)",
-          },
-          {
-            label: "Start Regelleistung (negativ)",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.02),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,165,0,0.5)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [5]
-          },
-          {
-            label: "Start Regelleistung (positiv)",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.98),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,165,0,0.5)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [20, 5]
-          },
-          {
-            label: "Reduzierung der Einspeisung",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.2),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,99,71,0.7)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [5]
-          },
-          {
-            label: "Einspeisung von Leistungsreserven",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.8),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,99,71,0.7)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [20, 5]
-          },
-          {
-            label: "Mögliche Stromausfälle",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 51.5),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255, 0, 0, 0.9)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [5]
-          },
-          {
-            label: "Trennung aller Solaranlagen vom Netz",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.0),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255, 0, 0, 0.9)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [20, 5]
-          },
-        ]
-      };
-    } else {
-      this.chart.data = {
-        labels: labeldata, 
-        datasets: [
-          {
-            label: "Netzfrequenz",
-            data: realdata,
-            backgroundColor: "rgba(0,0,0.7)",
-            borderColor: "rgba(0,0,0,0.6)"
-          },
-          {
-            label: "Richtwert",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(50,205,50,0.8)",
-            backgroundColor: "rgba(50,205,50,0.8)",
-          },
-          {
-            label: "Start Regelleistung (negativ)",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.02),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,165,0,0.5)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [5]
-          },
-          {
-            label: "Start Regelleistung (positiv)",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.98),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,165,0,0.5)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [20, 5]
-          },
-          {
-            label: "Reduzierung der Einspeisung",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 50.2),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,99,71,0.7)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [5]
-          },
-          {
-            label: "Einspeisung von Leistungsreserven",
-            data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.8),
-            fill: false,
-            pointRadius: 0.1,
-            borderColor: "rgba(255,99,71,0.7)",
-            backgroundColor: "rgb(251, 244, 226)",
-            borderDash: [20, 5]
-          },
-        ]
-      };
+      var blackoutsData = 
+        {
+          label: "Mögliche Stromausfälle",
+          data: Array.apply(null, realdata).map(Number.prototype.valueOf, 51.5),
+          fill: false,
+          pointRadius: 0.1,
+          borderColor: "rgba(255, 0, 0, 0.9)",
+          backgroundColor: "rgb(251, 244, 226)",
+          borderDash: [5],
+        }
+      var disconnectData = 
+        {
+          label: "Trennung aller Solaranlagen vom Netz",
+          data: Array.apply(null, realdata).map(Number.prototype.valueOf, 49.0),
+          fill: false,
+          pointRadius: 0.1,
+          borderColor: "rgba(255, 0, 0, 0.9)",
+          backgroundColor: "rgb(251, 244, 226)",
+          borderDash: [20, 5],
+        }
+      datasetData.push(blackoutsData, disconnectData)
     }
+
+    this.chart.data = {
+      labels: labeldata, 
+      datasets: datasetData,
+    };
     this.chart.update();
   }
 }
